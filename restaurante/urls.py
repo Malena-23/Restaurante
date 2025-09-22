e92a7f67bc03ce664493cc9c45d4787978a284b7
Malena De La Cruz <Malena-23>
2025-09-22 17:42:54 -0500

    Subir proyecto completo

---
+++ restaurante/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.main_index, name='main_index'),
+    path('accounts/', include('apps.accounts.urls')),
+    path('dashboard/', views.index_user, name='index_user'),
+]
